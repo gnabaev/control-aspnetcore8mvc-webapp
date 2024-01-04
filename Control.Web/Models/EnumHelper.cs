5c76b3c2ecb4f1be1b39bf78295428fb136f9736
gnabaev <gnabaev@gmail.com>
2024-01-05 00:38:27 +0300

    Добавлен класс с методом, получающим значение атрибута Display у перечислений. Отредактированы перечисления на всех представлениях.

---
+++ Control.Web/Models/EnumHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace Control.Web.Models
+{
+	public class EnumHelper
+	{
+		public static string GetDisplayName(Enum enumValue)
+		{
+			return enumValue.GetType()
+				.GetMember(enumValue.ToString())
+				.First()
+				.GetCustomAttribute<DisplayAttribute>()
+				.GetName();
+		}
+	}
+}
--- Control.Web/Models/IssueDiscipline.cs
+++ Control.Web/Models/IssueDiscipline.cs
@@ -1,12 +1,22 @@
-﻿namespace Control.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Control.Web.Models
 {
     public enum IssueDiscipline
     {
+        [Display(Name = "АР")]
         Architecture,
+        [Display(Name = "КР")]
         Structure,
+        [Display(Name = "ВиК")]
         WSS,
+        [Display(Name = "ОВиК")]
         HVAC,
+        [Display(Name = "ЭС")]
         Electrics,
+        [Display(Name = "CC")]
+        LowVoltage,
+        [Display(Name = "КИПиА")]
         Instrumentation
     }
 }
