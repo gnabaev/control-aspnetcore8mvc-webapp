9823febc268d6e1275f7df2d5f317f569d777057
gnabaev <gnabaev@gmail.com>
2024-01-05 22:02:31 +0300

    Создана базовая сущность комментария по замечанию.

---
+++ Control.Web/Models/IssueComment.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Control.Web.Models
+{
+    public class IssueComment
+    {
+        public int Id { get; set; }
+
+        public required string Text { get; set; }
+
+        public DateTime Date { get; set; }
+
+        // Реляционные отношения
+        public required string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public required User User { get; set; }
+
+        public required int IssueId { get; set; }
+
+        [ForeignKey(nameof(IssueId))]
+        public required Issue Issue { get; set; }
+    }
+}
